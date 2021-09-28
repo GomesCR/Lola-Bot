0d69e4f325df9cfaadc68322c223b2174a6c6fd7
GomesCR <paulocloudgomes@gmail.com>
2021-09-28 14:19:51 -0300

    Atualização de menu - 0.1 alpha

--- src/app.ts
+++ src/app.ts
@@ -19,11 +19,24 @@ manager.addDocument('pt', 'salve', 'saudacao');
 manager.addDocument('pt', 'quem é vc?', 'conhecimento');
 manager.addDocument('pt', 'quem é vc?', 'conhecimento');
 manager.addDocument('pt', 'Lola?', 'conhecimento');
+manager.addDocument('pt', 'menu', 'menu');
+manager.addDocument('pt', '1', '1');
+manager.addDocument('pt', '2', '2');
+manager.addDocument('pt', '3', '3');
+manager.addDocument('pt', '4', '4');
+
+
+
 
 
 // Train also the NLG
 manager.addAnswer('pt', 'saudacao', 'Oi, me chamo Lola, sou uma atendente virtual em fase de desenvolvimento!');
 manager.addAnswer('pt', 'conhecimento', 'Me chamo Lola, sua atendente virtual em fase de teste!');
+manager.addAnswer('pt', 'menu', 'Esse é o meu menu. Como posso lhe ajudar? Digite o número correspondente ao seu atendimento 1- Marcar horario 2- Comprar BOT 3- Falar com Paulo 4- voltar ao menu');
+manager.addAnswer('pt', '1', 'Você escolheu a opção 1, e temos disponivel no momento alguns hórarios, confira. INDISPONIVEL',);
+manager.addAnswer('pt', '2', 'Você escolheu a opção 2, para adquirir o bot, irei lhe mandar um link para o pagamento do bot, lembrando que o pagamento é mensal!!');
+manager.addAnswer('pt', '3', 'Você escolheu a opção 3, já estou colocando você em contato com o Paulo(NÃO FUNCIONAL AINDA)', );
+manager.addAnswer('pt', '4', 'Você voltou ao menu' );
 
 // Train and save the model.
 (async() => {
